25fb97772eb1592d6e9276196742bab3d661a24e
Brady Brown <brbrown@hubspot.com>
2023-10-09 22:37:29 -0400

    dynamically determine tsserver path on file open

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,57 +1,42 @@
--- local util = require("lspconfig/util")
---
-require("neodev").setup({})
--- example to setup lua_ls and enable call snippets
-require("lspconfig").lua_ls.setup({
-	settings = {
-		Lua = {
-			completion = {
-				callSnippet = "Replace",
-			},
-		},
-	},
-})
+local log = require("vim.lsp.log")
+local Job = require("plenary.job")
+local find_node_modules_ancestor = require("lspconfig").util.find_node_modules_ancestor
+local path_join = require("lspconfig").util.path.join
+local getTsServerPathForCurrentFile = require("asset-bender").getTsServerPathForCurrentFile
 
-local tsserverpath = vim.env.TSSERVER_PATH
-print("tsserverpath being calculated now: " .. tsserverpath)
+require("neodev").setup({})
+require("lspconfig").lua_ls.setup({})
+
+local tsserverpath = getTsServerPathForCurrentFile()
+
+print("Initializing lsp with tsserver version found from package.json:" .. tsserverpath)
+
 require("typescript-tools").setup({
 	settings = {
-		-- spawn additional tsserver instance to calculate diagnostics on it
 		separate_diagnostic_server = true,
-		-- "change"|"insert_leave" determine when the client asks the server about diagnostic
 		publish_diagnostic_on = "insert_leave",
-		-- array of strings("fix_all"|"add_missing_imports"|"remove_unused"|
-		-- "remove_unused_imports"|"organize_imports") -- or string "all"
-		-- to include all supported code actions
-		-- specify commands exposed as code_actions
 		expose_as_code_action = {},
-		-- string|nil - specify a custom path to `tsserver.js` file, if this is nil or file under path
-		-- not exists then standard path resolution strategy is applied
-
 		tsserver_path = tsserverpath,
 		tsserver_logs = "terse",
 		-- specify a list of plugins to load by tsserver, e.g., for support `styled-components`
 		-- (see 💅 `styled-components` support section)
 		tsserver_plugins = {},
-		-- this value is passed to: https://nodejs.org/api/cli.html#--max-old-space-sizesize-in-megabytes
-		-- memory limit in megabytes or "auto"(basically no limit)
 		tsserver_max_memory = "auto",
-		-- described below
 		tsserver_format_options = {},
 		tsserver_file_preferences = {},
-		-- mirror of VSCode's `typescript.suggest.completeFunctionCalls`
 		complete_function_calls = false,
 		include_completions_with_insert_text = true,
-		-- CodeLens
-		-- WARNING: Experimental feature also in VSCode, because it might hit performance of server.
-		-- possible values: ("off"|"all"|"implementations_only"|"references_only")
-		code_lens = "off",
-		-- by default code lenses are displayed on all referencable values and for some of you it can
-		-- be too much this option reduce count of them by removing member references from lenses
 		disable_member_code_lens = true,
 	},
 	on_attach = function()
-		require("asset-bender").check_start_javascript_lsp()
+		print("lsp attached")
+		local tsserverVersionForThisFile = getTsServerPathForCurrentFile()
+
+		if tsserverVersionForThisFile ~= tsserverpath then
+			vim.notify(
+				"You opened a file that requires a different tsserver version than what is currently being used."
+			)
+		end
 	end,
 })
 
