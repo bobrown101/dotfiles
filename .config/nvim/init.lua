2e26cb3ca93164719933adcbb38ab44db7e234d4
Brady Brown <brbrown@hubspot.com>
2024-09-09 09:59:14 -0400

    use /usr/bin/env to link to fish

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -36,20 +36,19 @@ require("lazy").setup({
         end,
     },
     {
-      'stevearc/oil.nvim',
-      ---@module 'oil'
-      ---@type oil.SetupOpts
-      opts = {},
-      -- Optional dependencies
-      dependencies = { { "echasnovski/mini.icons", opts = {} } },
-      -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-      config = function()
-          require("oil").setup({
-	      view_options = {
-	          show_hidden = true
-	      }
-	  })
-      end
+        "stevearc/oil.nvim",
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {},
+        -- Optional dependencies
+        dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+        config = function()
+            require("oil").setup({
+                view_options = {
+                    show_hidden = true,
+                },
+            })
+        end,
     },
     {
         "j-hui/fidget.nvim",
